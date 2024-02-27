23a49dfed624b81c57248cf65a5c5baf71409a7b
forsyth47 <joshuasuresh2006@gmail.com>
2024-02-27 22:01:50 +0530

    IniPush

--- setup.sh
+++ setup.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+stow
+i3
+bumblebee-status
+picom
+zsh
+terminator
+suckless-tools
+python-pywal
+imagemagick
+ohmyz.sh[]
 archinux-betterlockscreen
 
 git clone --depth=1 https://github.com/adi1090x/rofi.git
